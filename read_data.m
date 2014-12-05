c3b2114443ea2398c64e5533cfb72e9c7e195818
jiesongk <jiesongk@umich.edu>
2014-12-05 15:01:47 -0500

    read_data.m added to read in chunked data

---
+++ read_data.m
@@ -0,0 +1,18 @@
+function [fv] = read_data(dir_file_prefix, numfiles)
+    digit = length(int2str(numfiles));
+
+    for k = 1:numfiles
+      str = int2str(k);
+      if (length(str) < digit)
+          for i = 1:digit-length(str)
+              str = strcat('0', str);
+          end
+      end
+      myfilename = strcat(dir_file_prefix, str, '.csv')
+      if (k == 1)
+        fv = importdata(myfilename);
+      else
+        fv = vertcat(fv, importdata(myfilename));
+      end
+    end
+end
